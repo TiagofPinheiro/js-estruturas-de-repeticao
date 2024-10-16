cc92dd4d1eb28626e54ee87e827aa293ac31b3c6
Tiago Freitas Pinheiro <tiago8909@digitalcollegesul.local>
2024-10-16 11:59:53 -0300

    estrutura de condicao

---
+++ index.js
@@ -0,0 +1,14 @@
+let aluno1 = 'franck';
+let aluno2 = 'joel';
+let aluno3 = 'daniel';
+let alunos = ['camila', 'nayara', 'jessica'];
+alunos.push('frank')
+let mesNascimento = [9, 4, 4];
+mesNascimento.push(2)
+let ehEstudante = [false, true, false];
+ehEstudante.push(false)
+
+for(let i = 0; i <alunos.length; i++){
+    mensagens.innerHTML += `<h1>seja bem-vindo(a) ${alunos[i]}</h1>`;
+
+}
